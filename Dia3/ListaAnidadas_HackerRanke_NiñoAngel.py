7918f1da29b6f8d9e7ec4494e7487caba3f111a6
Angel <angeldavila0975@gmail.com>
2025-05-12 19:23:07 -0500

    Feat :bug: Class work

---
+++ Dia3/ListaAnidadas_HackerRanke_NiñoAngel.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    n= int(input())
+    estudiante={}
+    for _ in range(n):
+        name = score[0]
+        score = (input().split())
+        calificacion= list(map(float, score[1:]))
+        estudiante[name]=calificacion
+
+numero_studiantes= input()
+if numero_studiantes = sum
+
+
+
+
