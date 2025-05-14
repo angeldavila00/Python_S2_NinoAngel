2dcaac63f0646a96af3f85b1d99ff044af51c7a5
Angel <angeldavila0975@gmail.com>
2025-05-14 13:00:06 -0500

    feat :zap: Day 8

--- Dia1/pruebas.py
+++ Dia1/pruebas.py
@@ -1,102 +1,51 @@
 #####
 #PRUEBAS DE CODIGO##
-# Leer la cantidad de estudiantes
 # ##########################
-# #### Clase Dia 6 ######
+# #### Clase ######
 # ##########################
-# Diccionarios
-#Un DICccionario  es una colección de elementos , donde cada elementos insertado tiene una llave úntica, 
-# la cual va acompañada de un valor 
 
-##Diccionario con listas
-diccionarioRobusto={
-    "id":1,
-    "nombre":"Pedro",
-    "apellido":"Gómez",
-    "edad":25,
-    "telefonos":[{"codigo":57,"numero":3023019865,"tipo":"trabajo"}
-                 ,{"codigo":1,"numero":3983054625,"tipo":"personal"}]
+lista = [{
+        "color":"rosado",
+        "cosa":"pinza",
+        "textura":"corrugado",
+        "forma":"triangular"
+}, {
+        "color":"rojo",
+        "cosa":"carta",
+        "textura":"lisa",
+        "forma":"circular"
+}, {
+        "color":"azul",
+        "cosa":"mariposa",
+        "textura":"suave",
+        "forma":"cuadrado"
+}]
+
+
+#imprimir la textura de cada diccionario
+for i in range(len(lista)):
+    print(lista[i]["textura"])
+
+#crear una nueva lista despues de la nueva lista
+texturas=[]
+
+lista.append(texturas)
+
+abc={
+    "aa":1,
+    "bb":2
 }
-diccionarioRobusto2={
-    "id":2,
-    "nombre":"Corpus",
-    "apellido":"Bejarano",
-    "edad":27,
-    "telefonos":[{"codigo":58,"numero":2323057565,"tipo":"trabajo"}
-                 ,{"codigo":22,"numero":6857493658,"tipo":"personal"}]
-}
-listaRobusta=[]
-listaRobusta.append(diccionarioRobusto)
-listaRobusta.append(diccionarioRobusto2)
-print("")
-print("")
-print(listaRobusta)
-print("")
-print("")
-#print(listaRobusta[0]["telefonos"][1]['numero'])
+texturas.append(abc)
+print(texturas)
 
-for i in range(len(listaRobusta[0]["telefonos"])):
-    if(listaRobusta[0]["telefonos"][i]['tipo']=="trabajo"):
-        print(listaRobusta[0]["telefonos"][i]['numero'])
-print("")
-print("")
-numeroPrimeraPersona=listaRobusta[0]["telefonos"][1]['numero']
-tipoNumeroPP=listaRobusta[0]["telefonos"][1]['tipo']
-print(str(numeroPrimeraPersona)+ tipoNumeroPP)
 
-booleanito = True
-while(booleanito):
-    print("#################")
-    print("#### Librería de personas ####")
-    print("#################")
-    #CRUD (CREATE , READ , UPDATE & DELETE)
-    print("1. Crear Persona")
-    print("2. Mostrar todas las personas")
-    print("3. Mostrar a una persona individual")
-    print("4. Actualizar a una persona en específico")
-    print("5. Eliminar a una persona en específico")
-    print("6. Cerrar programa")
-    opcionUsuario=int(input("Escoja una opción (Numérica):"))
-    if(opcionUsuario==1):
-        print("#################")
-        print("#### Crear Persona ####")
-        print("#################")
-        diccionarioVacio={}
 
-    elif(opcionUsuario==2):
-        for i in range(len(listaRobusta)):
-            print("#################")
-            print("#### Persona #",i+1," ####")
-            print("#################")
-            print("ID:", listaRobusta[i]["id"])
-            print("Nombre:",listaRobusta[i]["nombre"])
-            print("Apellido:",listaRobusta[i]["apellido"])
-            print("Edad",listaRobusta[i]["edad"])
-            
-            for q in range(len(listaRobusta[i]["telefonos"])):
-                print("---------------------------")
-                print("Telefono#",q+1,":")
-                print("#### - Código:",listaRobusta[i]["telefonos"][q]["codigo"])
-                print("#### - Numero:",listaRobusta[i]["telefonos"][q]["numero"])
-                if(listaRobusta[i]["telefonos"][q]["tipo"] == "personal"):
-                    print("#### - Tipo: Es su número Personal")
-                else:
-                    print("#### - Tipo: Es su número de Trabajo")
-                
-                print("---------------------------")
-
-            
-            
-    elif(opcionUsuario==6):
-        print("Chaousssss")
-        booleanito=False
-    else:
-        print("No es una opción válida")
-
+#agregar una nueva key : nueva informacion
+lista[0]["tamaño"]="mediano"
+print (lista[0])
     
-    
-    
-#Desarrollado por Pedro Felipe Gómez : C.C-1.555.444.333
+print(lista[1]["cosa"])
+
 
 
 ## Desarrollado por : Angel Niño Davila 
