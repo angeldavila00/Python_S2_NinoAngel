78412de254f99e546e93c5553b8ed87562b562f9
Angel <angeldavila0975@gmail.com>
2025-05-15 14:05:41 -0500

    Feat :bug: New work day9

--- Trabajo/Trabajo_dia9.py
+++ Trabajo/Trabajo_dia9.py
@@ -1,5 +1,22 @@
 from funciones.funcionesGGDD import *
-listaArt=[]
+
 listaArt = abrirJSON()
 
-print (listaArt)
+booleanito = True
+
+while(booleanito):
+    listaArt=abrirJSON()
+    print("_________________________________")
+    print("Bienvenido a nuestra base de datos de artistas musicales")
+    print("_________________________________")
+    print("1. Registrar datos de un nuevo Artista")
+    print("2. Registro y gestion de los paises")
+    print("3. Registro de los generos musicales")
+    print("4. Mostrar todos los artistas de un pais y tiempo especifico")
+    print("5. Generacion de modulo de resportes")
+    opcionUsuario=int(input("Escoja una opción (Numérica):"))
+    if(opcionUsuario==1):
+        print("")
+        
+
+
--- Trabajo/funciones/funcionesGGDD.py
+++ Trabajo/funciones/funcionesGGDD.py
@@ -2,7 +2,7 @@ import json
 
 def abrirJSON():
     listaArtista=[]
-    with open("./data.json",'r') as openFile:
+    with open("./data/datos.json",'r') as openFile:
         listaArtista=json.load(openFile)
     return listaArtista
 
